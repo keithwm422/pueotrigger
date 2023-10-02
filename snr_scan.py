456fd50ceb0d8907d0aa84a746876c8567542692
keithwm422 <keithwm.422@gmail.com>
2023-10-01 21:43:30 -0500

    semi-working range of angles for snr scan

--- snr_scan.py
+++ snr_scan.py
@@ -10,8 +10,8 @@ import noise
 #pick phi, theta (incoming wave angle)
 #phi = 15
 #theta = -10
-phi=numpy.arange(0,51,10)
-theta=numpy.arange(-40,10,4)
+phi=numpy.arange(-5,51,10)
+theta=numpy.arange(-40,10,5)
 
 ## angle scan not yet implemented
 #theta_range = numpy.arange(-40, 10, 4)
@@ -57,18 +57,29 @@ if __name__=='__main__':
     print(phi.shape[0])
     print(type(theta.shape))
     delays=numpy.empty((phi.shape[0],theta.shape[0])) # declare shape based on theta and phi which are tuples
-    # access like delays[iphi,itheta] for example delays[1,12] 
-    waveforms=numpy.empty((phi.shape[0],theta.shape[0]))
-    timebase=numpy.empty((phi.shape[0],theta.shape[0]))
-    multiplier=numpy.empty((phi.shape[0],theta.shape[0]))
+    # access like delays[iphi,itheta] for example delays[1,12]
+
+    #waveforms=numpy.empty((phi.shape[0],theta.shape[0]))
+    #timebase=numpy.empty((phi.shape[0],theta.shape[0]))
+    #multiplier=numpy.empty((phi.shape[0],theta.shape[0]))
+    delays_list=[]
+    waveforms_list=[]
+    timebase_list=[]
+    multiplier_list=[]
+
     # we can loop over all incoming wave angles.
     phi_i=0
     theta_i=0
     while phi_i<len(phi):
         theta_i=0
         while theta_i<len(theta):
-            delays[phi_i,theta_i]=payload.getRemappedDelays(phi[phi_i], theta[theta_i], phi_sectors) # generate the delays per phi sector, and for each incoming wave angle phi[phi_i] and theta[theta_i]
-            waveforms[phi_i,theta_i], timebase[phi_i,theta_i], multiplier[phi_i,theta_i] = payload.getPayloadWaveforms(phi[phi_i], theta[theta_i], phi_sectors, impulse, (eplane, hplane), plot=False, downsample=True) # find the waveform at the payload for each
+            delays=payload.getRemappedDelays(phi[phi_i], theta[theta_i], phi_sectors) # generate the delays per phi sector, and for each incoming wave angle phi[phi_i] and theta[theta_i]
+            waveforms, timebase, multiplier = payload.getPayloadWaveforms(phi[phi_i], theta[theta_i], phi_sectors, impulse, (eplane, hplane), plot=False, downsample=True) # find the waveform at the payload for each
+            # must add to list for now
+            delays_list.append(delays)
+            waveforms_list.append(waveforms)
+            timebase_list.append(timebase)
+            multiplier_list.append(multiplier)
             theta_i+=1
         phi_i+=1
     
@@ -85,20 +96,23 @@ if __name__=='__main__':
 
     ## noise profile 2
     thermal_noise_v2 = noise.ThermalNoise(0.26, 1.05, filter_order=(10,10), v_rms=1.0, 
-                                       fbins=waveforms[0,0].shape[2], 
+                                       fbins=waveforms.shape[2], 
                                        time_domain_sampling_rate=aso_geometry.ritc_sample_step)
 
     noise_list=[] # list which now has entries that are different sizes...
     #noise_list.append(thermal_noise_v1.makeNoiseWaveform(ntraces=num_of_events_per_snr_step*waveforms.shape[0]*waveforms.shape[1]))
-    noise_list.append(thermal_noise_v2.makeNoiseWaveform(ntraces=num_of_events_per_snr_step*waveforms[0,0].shape[0]*waveforms[0,0].shape[1]))
+    noise_list.append(thermal_noise_v2.makeNoiseWaveform(ntraces=num_of_events_per_snr_step*waveforms.shape[0]*waveforms.shape[1]))
 
     # first pick phi_i and theta_i for testing...
     myphi=15
     mytheta=-10
-    phi_index=(numpy.abs(phi-myphi)).argmin
-    theta_index=(numpy.abs(theta-mytheta)).argmin
+    phi_index=numpy.argmin(numpy.abs(phi-myphi))
+    theta_index=numpy.argmin(numpy.abs(theta-mytheta))
+    #phi_i=0 and theta_i =0 is waveforms_list[0], phi_i=0 and theta_i=1 would be waveforms_list[1], phi_i=1 and theta_i=0 would be waveforms_list[len(theta)-1] so (phi_index(len_theta))-1 + theta_index
+    print(phi_index)
     print('your phi: ', phi[phi_index])
     print('your theta: ', theta[theta_index])
+    waveforms_index=(phi_index*len(theta))-1 + theta_index
     for j in range(len(noise_list)):
         hits=[]
         for snr in snr_scan:
@@ -107,7 +121,7 @@ if __name__=='__main__':
                 event_noise = numpy.reshape(numpy.real(noise_list[j][2][i*waveforms.shape[0]*waveforms.shape[1]:i*waveforms.shape[0]*waveforms.shape[1]+\
                                                                          waveforms.shape[0]*waveforms.shape[1]]), (waveforms.shape[0], waveforms.shape[1], waveforms.shape[2]))
                 
-                coh_sum, timebase_coh_sum  = trigger.coherentSum(waveforms*snr+event_noise, timebase, delays, ringmask=ringmask)
+                coh_sum, timebase_coh_sum  = trigger.coherentSum(waveforms_list[waveforms_index]*snr+event_noise, timebase_list[waveforms_index], delays_list[waveforms_index], ringmask=ringmask)
                 #print(coh_sum, timebase_coh_sum)
                 power,_ = trigger.powerSum(coh_sum/numpy.sqrt(waveforms.shape[0]*numpy.sum(ringmask)), window=window, step=step)
 
